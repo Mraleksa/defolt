d235130ade28d9eb1be7d71819179a68dd1fa09a
Mraleksa <a.gribanovskiy@gmail.com>
2017-06-01 01:21:59 +0300

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -1,7 +1,11 @@
 
 var unique = require('uniq');
-
+var fs = require("fs");
 
 var data = [1, 2, 2, 3, 4, 5, 5, 5, 6];
 
 console.log(unique(data));
+
+fs.writeFile("nest.csv", unique(data), function(err) {
+			console.log("file written");
+		});
