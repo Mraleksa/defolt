986584c61ab3fd304aa0ec8d71c137041f34d611
Mraleksa <a.gribanovskiy@gmail.com>
2017-06-01 01:18:56 +0300

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -1,59 +1,19 @@
-// This is a template for a Node.js scraper on morph.io (https://morph.io)
-
-var cheerio = require("cheerio");
-var request = require("request");
 var sqlite3 = require("sqlite3").verbose();
+var unique = require('uniq');
 
-function initDatabase(callback) {
-	// Set up sqlite database.
-	var db = new sqlite3.Database("data.sqlite");
-	db.serialize(function() {
-		db.run("CREATE TABLE IF NOT EXISTS data (name TEXT)");
-		callback(db);
-	});
-}
+// Open a database handle
+var db = new sqlite3.Database("data.sqlite");
+db.serialize(function() {
 
-function updateRow(db, value) {
-	// Insert some data.
-	var statement = db.prepare("INSERT INTO data VALUES (?)");
-	statement.run(value);
-	statement.finalize();
-}
+  // Create new table
+  db.run("CREATE TABLE IF NOT EXISTS data (title TEXT)");
 
-function readRows(db) {
-	// Read some data.
-	db.each("SELECT rowid AS id, name FROM data", function(err, row) {
-		console.log(row.id + ": " + row.name);
-	});
-}
+  // Insert a new record
+  var statement = db.prepare("INSERT INTO data(title) VALUES (?)");
+  statement.run( Math.round(Math.random()*100));
+  statement.finalize();
+});
 
-function fetchPage(url, callback) {
-	// Use request to read in pages.
-	request(url, function (error, response, body) {
-		if (error) {
-			console.log("Error requesting page: " + error);
-			return;
-		}
+var data = [1, 2, 2, 3, 4, 5, 5, 5, 6];
 
-		callback(body);
-	});
-}
-
-function run(db) {
-	// Use request to read in pages.
-	fetchPage("https://morph.io", function (body) {
-		// Use cheerio to find things in the page with css selectors.
-		var $ = cheerio.load(body);
-
-		var elements = $("div.media-body span.p-name").each(function () {
-			var value = $(this).text().trim();
-			updateRow(db, value);
-		});
-
-		readRows(db);
-
-		db.close();
-	});
-}
-
-initDatabase(run);
+console.log(unique(data));
